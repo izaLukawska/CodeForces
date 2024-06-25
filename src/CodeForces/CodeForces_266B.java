37ecc78addba1c09f015d07986c6ba14576b5bf1
Izabela Łukawska <lukawska.cf@gmail.com>
2024-06-25 16:29:34 +0200

    Code Forces 266B

---
+++ src/CodeForces/CodeForces_266B.java
@@ -0,0 +1,31 @@
+package CodeForces;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+//https://codeforces.com/problemset/problem/266/B
+
+public class CodeForces_266B {
+
+    public static String queue(int kids, int seconds, String sex) {
+        if (kids < 1 && kids > seconds) {
+            throw new IllegalArgumentException("Kids number must be bigger than 1 and less than seconds count");
+        } else if (seconds > 50 && seconds < kids) {
+            throw new IllegalArgumentException("Seconds count must be bigger than kids number but less than 50");
+        } else {
+            LinkedList<String> list = Arrays.stream(sex.split("")).collect(Collectors.toCollection(LinkedList::new));
+
+            for(int j = 0; j < seconds; ++j) {
+                for(int i = list.size() - 1; i > 0; --i) {
+                    if (((String)list.get(i)).equals("G") && ((String)list.get(i - 1)).equals("B")) {
+                        list.set(i, "B");
+                        list.set(i - 1, "G");
+                    }
+                }
+            }
+
+            return String.join("", list);
+        }
+    }
+}
