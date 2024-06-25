8abcabfaaf8f9661d02a9eca1b24728774f9e4ad
Izabela Łukawska <lukawska.cf@gmail.com>
2024-06-25 16:06:56 +0200

    Code Forces 236A

---
+++ src/CodeForces/CodeForces_236A.java
@@ -0,0 +1,16 @@
+package CodeForces;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// https://codeforces.com/problemset/problem/236/A
+
+public class CodeForces_236A {
+    public static String sexPrediction(String username) {
+        Set<String> result = new HashSet<>();
+        for (int i = 0; i < username.length(); i++) {
+            result.add(String.valueOf(username.charAt(i)));
+        }
+        return result.size() % 2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!";
+    }
+}
