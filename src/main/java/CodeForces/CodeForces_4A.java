74e0c95b2c6b67e52d506df38f297337b488f0bd
Izabela Łukawska <lukawska.cf@gmail.com>
2024-09-26 17:11:40 +0200

    changed the code

--- src/main/java/CodeForces/CodeForces_4A.java
+++ src/main/java/CodeForces/CodeForces_4A.java
@@ -4,7 +4,7 @@ package CodeForces;
 
 public class CodeForces_4A {
     public static String canDivideInTwoParts(int weight) {
-        if (weight < 0 || weight > 100) {
+        if (weight < 1 || weight > 100) {
             throw new IllegalArgumentException("Weight must be in the range 1 to 100.");
         }
         return weight % 2 == 0 && weight > 2 ? "YES" : "NO";
